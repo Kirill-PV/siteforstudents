017d8db5fd78db2ccea6407db155e7678153f84a
username <mail@ex.com>
2025-08-16 17:50:47 +0000

    start projet

---
+++ siteforstudents/homepage/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse('Страница приложения Homepage')
+
+def categories(request):
+    return HttpResponse('<h1>Категории приложения Homepage</h1>')
--- siteforstudents/siteforstudents/urls.py
+++ siteforstudents/siteforstudents/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
+from homepage.views import *
+from django.urls import include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('homepage/', include('homepage.urls')),
+
+    ]
