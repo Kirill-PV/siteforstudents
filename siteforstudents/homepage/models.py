c6bcdfb6f37fda8a6d2ccbd876a4f62d5d0a1943
username <mail@ex.com>
2025-08-21 16:20:11 +0000

    main root

--- siteforstudents/homepage/models.py
+++ siteforstudents/homepage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Content(models.Model):
+     title = models.CharField(max_length=255)
+     content = models.TextField(blank=True)
+     file = models.FileField(upload_to='PDF/%Y/%m/%d/')# посмотреть как настроить доступ к pdf
+     time_create = models.DateTimeField(auto_now_add=True)
+     time_update = models.DateTimeField(auto_now=True)
+     is_published = models.BooleanField(default=True)
