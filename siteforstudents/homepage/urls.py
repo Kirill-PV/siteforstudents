ba25ecd9b490856c9aea5730dbb9a3c919634c8d
username <mail@ex.com>
2025-10-21 13:46:50 +0000

    main root

--- siteforstudents/homepage/urls.py
+++ siteforstudents/homepage/urls.py
@@ -4,8 +4,11 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'), # http://127.0.0.1:8000/homepage/
-    path('cats/<slug:cat>/', categories),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
     path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+
+
 
 ]
